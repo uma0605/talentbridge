37a9fd58e45a2ccf4ba2bb4ae114645c32db423a
uma0605 <umaheshwari329@gmail.com>
2026-06-29 15:18:23 +0530

    Update and rename adminAuthController.js to companyauth.js

--- models/models/controllers/companyauth.js
+++ models/models/controllers/companyauth.js
@@ -3,7 +3,7 @@ import Company from '../models/Company.js';
 
 const generateToken = (id, role) => {
   return jwt.sign({ id, role }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
+    expiresIn: process.env.JWT_EXPIRE
   });
 };
 
